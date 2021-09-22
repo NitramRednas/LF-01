4b7027026b5153d74b96e8c3edd74d8ba0061da4
martin307 <martin307@schule.bremen.de>
2021-09-22 10:59:33 +0200

    Additionsrechner "Mein zweites Programm" Optisch verfeinert

---
+++ Scratch2Java/src/ZweitesProgrammUebung.java
@@ -0,0 +1,30 @@
+
+
+public class ZweitesProgrammUebung {
+
+    public static void main (String[] args)
+
+
+{       //Deklaration
+
+        int zahl1;
+        int zahl2;
+        int ergebnis;
+
+
+        //Initialisierung(erstmalige Wertzuweisung) bzw. Wertzuweisung
+
+        zahl1=5;
+        zahl2=4;
+
+        //Rechnen
+
+        ergebnis=zahl1 + zahl2;
+
+        //Ausgabe des Ergebnisses
+
+        System.out.println("Ich bin mir nicht sicher, aber ich denke es ist");
+        System.out.print(ergebnis);
+
+        }
+}
